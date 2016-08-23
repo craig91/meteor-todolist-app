08d801ba8b01e3fbb03a14842bcaa230849aef9f
craig91 <craigdejean1@gmail.com>
2016-08-22 23:53:55 -0400

    Syntax Corrections

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 import Task from './Task.jsx';
 
-//App comment - represents the whole app
+// App component - represents the whole app
 export default class App extends Component {
-  getTasks(){
+  getTasks() {
     return [
       { _id: 1, text: 'This is task 1' },
       { _id: 2, text: 'This is task 2' },
@@ -12,24 +12,23 @@ export default class App extends Component {
     ];
   }
 
- renderTasks() {
-   return this.getTasks().map((task) => (
-       <Task key={task._id} task={task} />
-     ));
- }
+  renderTasks() {
+    return this.getTasks().map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
 
-render() {
-  return (
-    <div className="container">
-      <header>
-        <h1>Todo List</h1>
-      </header>
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
 
-
-     <ul>
-       {this.renderTasks()}
-     </ul>
-   </div>
-   );
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
   }
 }
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -4,12 +4,12 @@ import React, { Component, PropTypes } from 'react';
 export default class Task extends Component {
   render() {
     return (
-      <li>(this.props.task.text)</li>
+      <li>{this.props.task.text}</li>
     );
   }
 }
 
-Tasks.propTypes = {
+Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
   task: PropTypes.object.isRequired,
